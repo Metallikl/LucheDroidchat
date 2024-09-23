c5b6bb8e7a02dc9e9c329f006998446ae3c4c9ac
DanielLuche <daniel.betteloni@gmail.com>
2024-09-23 13:38:32 -0300

    modulo 2, aula criando annotatedString

--- app/src/main/java/com/dluche/luchedroidchat/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/dluche/luchedroidchat/ui/feature/signin/SignInScreen.kt
@@ -6,19 +6,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -30,19 +39,22 @@ import com.dluche.luchedroidchat.ui.theme.LucheDroidChatTheme
 
 @Composable
 fun SignInRoute(
-    viewModel: SignInViewModel = viewModel()
+    viewModel: SignInViewModel = viewModel(),
+    navigateToSignUp: () -> Unit
 ) {
     val formState = viewModel.formState
     SignInScreen(
         formState = formState,
-        onFormEvent = viewModel::onFormEvent
+        onFormEvent = viewModel::onFormEvent,
+        onRegisterClick = navigateToSignUp
     )
 }
 
 @Composable
 fun SignInScreen(
     formState: SignInFormState,
-    onFormEvent: (SignInFormEvent) -> Unit
+    onFormEvent: (SignInFormEvent) -> Unit,
+    onRegisterClick: ()-> Unit
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -104,6 +116,45 @@ fun SignInScreen(
             modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
             isLoading = formState.isLoading
         )
+
+        Spacer(modifier = Modifier.height(56.dp))
+
+        val noAccountText = stringResource(id = R.string.feature_login_no_account)
+        val registerText = stringResource(id = R.string.feature_login_register)
+        val noAccountRegisterText = "$noAccountText $registerText"
+        val annotatedString = buildAnnotatedString {
+            val registerTextStartIndex = noAccountRegisterText.indexOf(registerText)
+            val registerTextEndIndex = registerTextStartIndex + registerText.length
+            append(noAccountRegisterText)
+            addStyle(
+                style = SpanStyle(
+                    color = Color.White
+                ),
+                start = 0,
+                end = registerTextStartIndex
+            )
+
+            addLink(
+                clickable = LinkAnnotation.Clickable(
+                    tag = "register_text",
+                    styles = TextLinkStyles(
+                        style = SpanStyle(
+                            color = MaterialTheme.colorScheme.primary,
+                            textDecoration = TextDecoration.Underline
+                        )
+                    ),
+                    linkInteractionListener = {
+                        onRegisterClick()
+                    }
+                ),
+                start = registerTextStartIndex,
+                end = registerTextEndIndex
+            )
+        }
+
+        Text(
+            text = annotatedString,
+        )
     }
 }
 
@@ -113,9 +164,9 @@ private fun SignInScreenPreview() {
     LucheDroidChatTheme {
         SignInScreen(
             formState = SignInFormState(),
-            onFormEvent = {}
+            onFormEvent = {},
+            onRegisterClick = {}
         )
     }
-
 }
 
--- app/src/main/java/com/dluche/luchedroidchat/ui/theme/Theme.kt
+++ app/src/main/java/com/dluche/luchedroidchat/ui/theme/Theme.kt
@@ -53,10 +53,10 @@ fun LucheDroidChatTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
