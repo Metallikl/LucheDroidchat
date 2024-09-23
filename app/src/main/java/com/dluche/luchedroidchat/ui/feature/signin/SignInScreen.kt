c49d27d46025ac9ac21f504159b0a351ece07fd0
DanielLuche <daniel.betteloni@gmail.com>
2024-09-23 08:43:48 -0300

    modulo 2 aula finaliza primary button

--- app/src/main/java/com/dluche/luchedroidchat/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/dluche/luchedroidchat/ui/feature/signin/SignInScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -22,6 +26,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dluche.luchedroidchat.R
+import com.dluche.luchedroidchat.ui.components.PrimaryButton
 import com.dluche.luchedroidchat.ui.components.PrimaryTextField
 import com.dluche.luchedroidchat.ui.theme.BackgroundGradient
 import com.dluche.luchedroidchat.ui.theme.LucheDroidChatTheme
@@ -38,15 +43,17 @@ fun SignInScreen() {
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(BackgroundGradient)
             .padding(8.dp)
+            .imePadding()
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = null
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(78.dp))
 
         var email by remember {
             mutableStateOf("")
@@ -55,8 +62,6 @@ fun SignInScreen() {
             mutableStateOf("")
         }
 
-        Spacer(modifier = Modifier.height(64.dp))
-
         PrimaryTextField(
             value = email,
             onValueChange = {
@@ -64,11 +69,12 @@ fun SignInScreen() {
             },
             placeholder = stringResource(id = R.string.feature_login_email),
             modifier = Modifier
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
             leadingIcon = R.drawable.ic_envelope,
             keyboardType = KeyboardType.Email
         )
-        Spacer(modifier = Modifier.height(32.dp))
+
+        Spacer(modifier = Modifier.height(14.dp))
 
         PrimaryTextField(
             value = passowrd,
@@ -76,11 +82,24 @@ fun SignInScreen() {
                 passowrd = it
             },
             modifier = Modifier
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
             placeholder = stringResource(id = R.string.feature_login_password),
             keyboardType = KeyboardType.Password,
             leadingIcon = R.drawable.ic_lock,
             imeAction = ImeAction.Done
+        )
+
+        Spacer(modifier = Modifier.height(98.dp))
+
+        var isLoading by remember {
+            mutableStateOf(false)
+        }
+
+        PrimaryButton(
+            text = stringResource(id = R.string.feature_login_button),
+            onClick = { isLoading = !isLoading },
+            modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
+            isLoading = isLoading
         )
     }
 }
