93cae53c81ec0dfe4b2dd1a9042ebfd71c9e5216
DanielLuche <daniel.betteloni@gmail.com>
2025-01-21 08:40:32 -0300

    modulo 3 : Implementa dispatcher coroutine

--- app/src/main/java/com/dluche/luchedroidchat/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/dluche/luchedroidchat/data/repository/AuthRepositoryImpl.kt
@@ -1,25 +1,31 @@
 package com.dluche.luchedroidchat.data.repository
 
+import com.dluche.luchedroidchat.data.IoDispatcher
 import com.dluche.luchedroidchat.data.network.NetworkDataSource
 import com.dluche.luchedroidchat.data.network.model.AuthRequest
 import com.dluche.luchedroidchat.data.network.model.CreateAccountRequest
 import com.dluche.luchedroidchat.model.CreateAccount
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val networkDataSource: NetworkDataSource
+    private val networkDataSource: NetworkDataSource,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : AuthRepository {
     override suspend fun signUp(createAccount: CreateAccount): Result<Unit> {
-        return runCatching {
-            networkDataSource.signUp(
-                request = CreateAccountRequest(
-                    username = createAccount.username,
-                    password = createAccount.password,
-                    firstName = createAccount.firstName,
-                    lastName = createAccount.lastName,
-                    profilePictureId = createAccount.profilePictureId
+        return withContext(dispatcher) {
+            runCatching {
+                networkDataSource.signUp(
+                    request = CreateAccountRequest(
+                        username = createAccount.username,
+                        password = createAccount.password,
+                        firstName = createAccount.firstName,
+                        lastName = createAccount.lastName,
+                        profilePictureId = createAccount.profilePictureId
+                    )
                 )
-            )
+            }
         }
     }
 
