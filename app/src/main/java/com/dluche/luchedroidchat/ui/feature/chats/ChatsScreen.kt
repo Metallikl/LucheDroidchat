69608d442bf3ec77188adcfcd42bb7b9dcb0b4d7
DanielLuche <daniel.betteloni@gmail.com>
2025-02-15 17:11:55 -0300

    Implementando o layout da tela de Chats

--- app/src/main/java/com/dluche/luchedroidchat/ui/feature/chats/ChatsScreen.kt
+++ app/src/main/java/com/dluche/luchedroidchat/ui/feature/chats/ChatsScreen.kt
@@ -2,19 +2,28 @@
 
 package com.dluche.luchedroidchat.ui.feature.chats
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dluche.luchedroidchat.R
 import com.dluche.luchedroidchat.ui.components.ChatItem
 import com.dluche.luchedroidchat.ui.theme.Grey1
 import com.dluche.luchedroidchat.ui.theme.LucheDroidChatTheme
@@ -30,17 +39,43 @@ fun ChatsScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Ola, Dev")
-                }
+                    Text(
+                        text = AnnotatedString.fromHtml(
+                            stringResource(
+                                R.string.feature_chats_greeting,
+                                "Daniel"
+                            )
+                        ),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                expandedHeight = 100.dp
             )
-        }
+        },
+        containerColor = MaterialTheme.colorScheme.primary,
     ){ paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .padding(paddingValues)
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = MaterialTheme.shapes.extraLarge.copy(
+                        bottomStart = CornerSize(0.dp),
+                        bottomEnd = CornerSize(0.dp)
+                    )
+                )
+                .clip(
+                    shape = MaterialTheme.shapes.extraLarge.copy(
+                        bottomStart = CornerSize(0.dp),
+                        bottomEnd = CornerSize(0.dp)
+                    )
+                )
                 .fillMaxSize(),
             contentPadding = PaddingValues(horizontal = 16.dp)
-            
         ) {
             items(100){
                 ChatItem()
